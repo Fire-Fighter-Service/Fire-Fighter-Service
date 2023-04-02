6ad5807824b4790c3ba346609a855538d59849ef
Asikur Rahman Sumon <97869436+AR-Sumon@users.noreply.github.com>
2023-04-02 23:59:02 +0600

    Add files via upload
    Final

--- views.py
+++ views.py
@@ -228,39 +228,26 @@ def allRequest(request):
     firereport = Firereport.objects.all()
     return render(request, 'admin/allRequest.html', locals())
 
+def changePassword(request):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    error = ""
+    user = request.user
+    if request.method == "POST":
+        o = request.POST['oldpassword']
+        n = request.POST['newpassword']
+        try:
+            u = User.objects.get(id=request.user.id)
+            if user.check_password(o):
+                u.set_password(n)
+                u.save()
+                error = "no"
+            else:
+                error = 'not'
+        except:
+            error = "yes"
+    return render(request, 'admin/changePassword.html', locals())
 
-
-
-
-dkfjdlkfjdfdf
-fdafjd;klfjasd;fjad
-fdfdf
-FileNotFoundErrordfdfdf
-
-fdfdfdfdfdfdferetrhtutyujyuyutrhgsgh
-shtruryujggffrrttryutikrksghj
-serutikykwsrtrutyj
-stkskyktkmyk
-SystemError
-
-
-mykyky
-global
-globalgg
-globalg
-gg
-
-gteteete
-
-KeyboardInterruptkukuku
-
-
-ukkuuuuuuuu
-
-
-ererererer
-dfsdfsfsf
-
-
-f
-ssssssssffs
+def Logout(request):
+    logout(request)
+    return redirect('index')
