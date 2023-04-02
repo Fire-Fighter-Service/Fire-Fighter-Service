d594759e3d6bdd5acb689173948fd2c5c3741ae1
Rifa Tasfia <97869443+tasfiarifa@users.noreply.github.com>
2023-04-03 03:01:50 +0600

    Update models.py

--- models.py
+++ models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Teams(models.Model):
     teamName = models.CharField(max_length=200, null=True)
     teamLeaderName = models.CharField(max_length=250, null=True)
-    teamLeadMobno = models.CharField(max_length=15, null=True)
+    teamLeadMobno = models.CharField(max_length=20, null=True)
     teamMembers = models.CharField(max_length=300, null=True)
-    postingDate = models.DateTimeField(auto_now_add=True)
+    #postingDate = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.teamName
@@ -32,7 +32,7 @@ class Teams(models.Model):
 class Firetequesthistory(models.Model):
    firereport = models.ForeignKey(Firereport, on_delete=models.CASCADE, null=True)
    status = models.CharField(max_length=200, null=True)
-     remark = models.CharField(max_length=250, null=True)
+     #remark = models.CharField(max_length=250, null=True)
     postingDate = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
