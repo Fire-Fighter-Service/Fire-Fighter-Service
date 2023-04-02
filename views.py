50e1a48c550e3e87c4eaafa2583235f7f63820fe
UnizaArpa <98277794+UnizaArpa@users.noreply.github.com>
2023-04-03 02:21:20 +0600

    Update views.py

--- views.py
+++ views.py
@@ -8,8 +8,9 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 # Create your views here.
+
 #arpa
-def index(request):
+def index(request): 
     return render(request, 'index.html')
 #afrin
 def reporting(request):
